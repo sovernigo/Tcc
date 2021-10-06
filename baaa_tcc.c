5c3505a2c310bb0c1f4712f23d255aa6f53a9de4
sovernigo <felipessovernigo@hotmail.com>
2021-10-06 16:54:41 -0400

    Tentando orrigir o problema na falha de segmentação ao alocar vetores

--- baaa_tcc.c
+++ baaa_tcc.c
@@ -10,26 +10,27 @@
 
 FILE *arqIn;
 
-void read(char *entrada); // funcao que realiza a leitura da entrada
+// void read(char *entrada); // funcao que realiza a leitura da entrada
 
-int **AlocaMatriz(int num_Itens, int n_Colonias);
-void ini_Colonia(int **cabeca, int tamanho, int n_Itens);
+void AlocaMatriz(int num_Itens, int n_Colonias);
 void inserir_Sep(FILE *arquivo, int n_Itens, int n_Rec);
-void LiberaMatriz(int **colonia, int num_Itens);
+void ini_Colonia(int **cabeca, int tamanho, int n_Itens);
+void LiberaMatriz(int **colonia, int num_Itens, int num_Rec);
 
 
-double *tp_Recurso, *p_Recurso, *lim_Recurso;
+int *tp_Recurso, *p_Recurso, *lim_Recurso;
+int **colonia;
 
 int main(int argc, char *argv[]){
 
   int num_Itens, num_Rec, best_Sol, total_Val;
-  int j = 0, cont = 0;
+  int cont = 0;
 
   char *entrada = argv[1];
-  int n_Testes = argv[2];
-  int n_Colonias = argv[3];
+  int n_Testes = atoi(argv[2]) - 48;
+  int n_Colonias = atoi(argv[3]) - 48;
 
-  int **colonia;
+  printf("%d %d\n", n_Testes, n_Colonias);
 
   arqIn = fopen(entrada, "r");
 
@@ -45,9 +46,12 @@ int main(int argc, char *argv[]){
   }
 
   total_Val = num_Itens * num_Rec + num_Itens + num_Rec;
-  colonia = AlocaMatriz(num_Itens, n_Colonias);
+  printf("%d", total_Val);
 
   inserir_Sep(arqIn, num_Itens, num_Rec);
+
+
+  AlocaMatriz(num_Itens, n_Colonias);
 
   while(cont < n_Testes){
 
@@ -55,9 +59,10 @@ int main(int argc, char *argv[]){
 
     cont++;
   }
-  fclose(arqIn);
 
-  LiberaMatriz(colonia, num_Itens);
+  LiberaMatriz(colonia, num_Itens, num_Rec);
+
+  fclose(arqIn);
 
 }
 
@@ -86,71 +91,72 @@ int main(int argc, char *argv[]){
 }*/
 
 void inserir_Sep(FILE *arquivo, int n_Itens, int n_Rec){
-  double valor;
+  int valor;
 
   int cont = 0;
-  int i, j = 0;
+  int i, j, k = 0;
 
-  tp_Recurso = (double *) malloc(n_Itens * sizeof(double*));
-  p_Recurso = (double *) malloc((n_Itens * n_Rec) * sizeof(double*));
-  lim_Recurso = (double *) malloc(n_Rec * sizeof(double*));
+  tp_Recurso = (int *) malloc(n_Itens * sizeof(int));
+  p_Recurso = (int *) malloc((n_Itens * n_Rec) * sizeof(int));
+  lim_Recurso = (int *) malloc(n_Rec * sizeof(int));
 
-  while(fscanf(arqIn,"%lf", &valor) != EOF){
+  while(fscanf(arqIn,"%d", &valor) != EOF){
 
-    if(cont < n_Itens){
-      tp_Recurso[cont] = valor;
+    if(cont < n_Rec){
+      continue;
     }
-    else if(n_Itens <= cont < (n_Itens + 1)* n_Rec) {
-      p_Recurso[i] = valor;
+    else if(cont >= n_Rec && cont < n_Itens){
+      tp_Recurso[i] = valor;
       i++;
     }
-    else {
-      lim_Recurso[j] = valor;
+    else if(cont >= n_Itens && cont < (n_Itens * (n_Rec + 1))) {
+      p_Recurso[j] = valor;
       j++;
+    }
+    else if(cont >= (n_Itens * (n_Rec + 1))) {
+      lim_Recurso[k] = valor;
+      k++;
     }
     cont++;
   }
 }
 
-int **AlocaMatriz(int num_Itens, int n_Colonias){
-  int **colonia;
-  int i;
+void AlocaMatriz(int num_Itens, int n_Colonias){
 
-  colonia = (int **) malloc(num_Itens * sizeof(int**));
-  if(colonia == NULL){
-    printf("Memoria insuficiente.\n");
-    exit(1);
-  }
+  int i, j;
+
+  colonia = malloc(num_Itens * sizeof(int*));
+
   for(i = 0; i < num_Itens; i++){
-    colonia[i] = (int *)malloc(n_Colonias * sizeof(int));
-    if(colonia[i] == NULL){
-      printf("Memoria insuficiente.\n");
-      exit(1);
+    colonia[i] =  malloc(n_Colonias * sizeof(int));
+  }
+
+  for(i = 0; i < num_Itens; i++){
+    for(j = 0; j < n_Colonias; j++){
+      colonia[i][j] = 0;
     }
   }
-  return colonia;
 }
 
-void LiberaMatriz(int **colonia, int num_Itens){
-  int i;
-  for(i = 0; i < num_Itens; i++)
-    free(colonia[i]);
+void LiberaMatriz(int **colonia, int num_Itens, int num_Rec){
+
   free(colonia);
+  free(tp_Recurso);
+  free(p_Recurso);
+  free(lim_Recurso);
+
 }
 
 void ini_Colonia(int **cabeca, int tamanho, int n_Itens){
-  int i, random;
+  int i;
 
   while(i < tamanho){
-    random = rand();
-
-    if(random % 2 == 0){
-      cabeca[i] = 0;
-    }
-    else {
-      cabeca[i] = 1;
-    }
+    cabeca[i] = rand() % 2;
     i++;
   }
   return;
 }
+
+/*checa_Validade(){
+
+}*/
