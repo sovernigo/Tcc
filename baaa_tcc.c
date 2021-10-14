561807427114aa6fc7a9f9bfa3804cc833d0443f
sovernigo <54715784+sovernigo@users.noreply.github.com>
2021-10-14 03:33:54 -0400

    Add files via upload

--- baaa_tcc.c
+++ baaa_tcc.c
@@ -1,21 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
 
-/*typedef struct mochila {
-  double atributo; // matriz dimenciona a mochila do problema de forma dinamica
-  struct mochila *prox;
-}knapsack;*/
 
 FILE *arqIn;
 
-// void read(char *entrada); // funcao que realiza a leitura da entrada
-
-void AlocaMatriz(int num_Itens, int n_Colonias);
-void inserir_Sep(FILE *arquivo, int n_Itens, int n_Rec);
-void ini_Colonia(int **cabeca, int tamanho, int n_Itens);
-void LiberaMatriz(int **colonia, int num_Itens, int num_Rec);
+void AlocaMatriz(int, int);
+void inserir_Sep(FILE *, int, int);
+void ini_Colonia(int **, int, int);
+void LiberaMatriz(int **, int);
+void checa_Validade(int **, int, int);
 
 
 int *tp_Recurso, *p_Recurso, *lim_Recurso;
@@ -26,11 +22,11 @@ int main(int argc, char *argv[]){
   int num_Itens, num_Rec, best_Sol, total_Val;
   int cont = 0;
 
-  char *entrada = argv[1];
-  int n_Testes = atoi(argv[2]) - 48;
-  int n_Colonias = atoi(argv[3]) - 48;
+  char entrada[40];
 
-  printf("%d %d\n", n_Testes, n_Colonias);
+  strcpy(entrada, argv[1]);
+  int n_Testes = atoi(argv[2]);
+  int n_Colonias = atoi(argv[3]);
 
   arqIn = fopen(entrada, "r");
 
@@ -41,106 +37,68 @@ int main(int argc, char *argv[]){
 
   fscanf(arqIn, "%d %d %d", &num_Itens, &num_Rec, &best_Sol);
 
-  if(arqIn == NULL){
-    return 0;
-  }
 
   total_Val = num_Itens * num_Rec + num_Itens + num_Rec;
-  printf("%d", total_Val);
+
 
   inserir_Sep(arqIn, num_Itens, num_Rec);
-
 
   AlocaMatriz(num_Itens, n_Colonias);
 
   while(cont < n_Testes){
 
-    ini_Colonia(colonia, total_Val, num_Itens);
+    ini_Colonia(colonia, n_Colonias, num_Itens);
 
     cont++;
   }
 
-  LiberaMatriz(colonia, num_Itens, num_Rec);
+  LiberaMatriz(colonia, n_Colonias);
 
   fclose(arqIn);
 
 }
 
-/*void inserir(knapsack **cabeca, FILE *arquivo, int n_Itens, int n_Rec){
-  knapsack *noatual, *novo;
-  double valor;
-
-  while(fscanf(arqIn,"%lf", &valor) != EOF){
-
-    if(*cabeca == NULL){
-      *cabeca = (knapsack *) malloc(sizeof(knapsack));
-      (*cabeca)->atributo = valor;
-      (*cabeca)->prox = NULL;
-    }
-    else {
-      noatual = *cabeca;
-      while(noatual->prox != NULL){
-          noatual = noatual->prox;
-      }
-      novo = (knapsack *)malloc(sizeof(knapsack));
-      novo->atributo = valor;
-      novo->prox = NULL;
-      noatual->prox = novo;
-    }
-  }
-}*/
-
-void inserir_Sep(FILE *arquivo, int n_Itens, int n_Rec){
-  int valor;
-
-  int cont = 0;
-  int i, j, k = 0;
+void inserir_Sep(FILE *arquivo, int n_Itens, int n_Rec){ // função está dando falha na segmentação
 
   tp_Recurso = (int *) malloc(n_Itens * sizeof(int));
   p_Recurso = (int *) malloc((n_Itens * n_Rec) * sizeof(int));
   lim_Recurso = (int *) malloc(n_Rec * sizeof(int));
 
-  while(fscanf(arqIn,"%d", &valor) != EOF){
+  for (int j = 0; j < n_Itens; j++)
+	fscanf(arquivo, "%d", &(tp_Recurso[j]));
 
-    if(cont < n_Rec){
-      continue;
-    }
-    else if(cont >= n_Rec && cont < n_Itens){
-      tp_Recurso[i] = valor;
-      i++;
-    }
-    else if(cont >= n_Itens && cont < (n_Itens * (n_Rec + 1))) {
-      p_Recurso[j] = valor;
-      j++;
-    }
-    else if(cont >= (n_Itens * (n_Rec + 1))) {
-      lim_Recurso[k] = valor;
-      k++;
-    }
-    cont++;
-  }
+  //Leitura da matriz de pesos
+  for (int j = 0; j < n_Rec; j++)
+	 for (int k = 0; k < n_Itens; k++)
+	  fscanf(arquivo, "%d", &(p_Recurso[j * n_Itens + k]));
+
+  //Leitura do vetor de restrições
+  for (int j = 0; j < n_Rec; j++)
+	fscanf(arquivo, "%d", &(lim_Recurso[j]));
 }
 
 void AlocaMatriz(int num_Itens, int n_Colonias){
 
   int i, j;
 
-  colonia = malloc(num_Itens * sizeof(int*));
+  colonia = (int**) malloc(n_Colonias * sizeof(int*));
 
-  for(i = 0; i < num_Itens; i++){
-    colonia[i] =  malloc(n_Colonias * sizeof(int));
+  for(i = 0; i < n_Colonias; i++){
+    colonia[i] = (int*) malloc(num_Itens * sizeof(int));
   }
 
-  for(i = 0; i < num_Itens; i++){
-    for(j = 0; j < n_Colonias; j++){
-      colonia[i][j] = 0;
-    }
-  }
 }
 
-void LiberaMatriz(int **colonia, int num_Itens, int num_Rec){
+void LiberaMatriz(int **colonia, int n_Colonias){
 
+
+  //Libera cada linha da matriz
+  for (int i = 0; i < n_Colonias; i++)
+	free(colonia[i]);
+
+  //Libera a matriz
   free(colonia);
+
   free(tp_Recurso);
   free(p_Recurso);
   free(lim_Recurso);
@@ -148,15 +106,17 @@ void LiberaMatriz(int **colonia, int num_Itens, int num_Rec){
 }
 
 void ini_Colonia(int **cabeca, int tamanho, int n_Itens){
-  int i;
+  int i, j;
 
-  while(i < tamanho){
-    cabeca[i] = rand() % 2;
-    i++;
+  for (i = 0; i < tamanho; i++){
+	  for (j = 0; j < n_Itens; j++){
+      cabeca[i][j] = rand() % 2;
+      printf("%d ", cabeca[i][j]);
+    }
+    printf("\n");
   }
-  return;
 }
 
-/*checa_Validade(){
+void checa_Validade(int **cabeca, int tamanho, int n_Itens){
 
-}*/
+}
