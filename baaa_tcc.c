eb62a4cf7f31065da27a0e3e464a1e8b8cf22212
sovernigo <54715784+sovernigo@users.noreply.github.com>
2022-10-19 07:13:14 -0400

    Alterações de qualidade

--- baaa_tcc.c
+++ baaa_tcc.c
@@ -8,14 +8,14 @@
 
 FILE *arqIn;
 
-void AlocaMatriz(int, int);
-void inserir_Sep(FILE *, int, int);
-void ini_Colonia(int, int);
-void LiberaMatriz(int);
-void checa_Validade(int, int, int);
-void fit(int, int);
-void prep(int);
-void size(int);
+void AlocaMatriz();
+void inserir_Sep(FILE *);
+void ini_Colonia();
+void LiberaMatriz();
+void checa_Validade();
+void calcula_Fitness(int);
+void prep();
+void size();
 
 int *tp_Recurso, *p_Recurso, *lim_Recurso;
 int **colonia;
@@ -23,20 +23,21 @@ int *fit_Colonia;
 float *fric_surf;
 float *atual_Size;
 float *update_cosc;
+int num_Itens, num_Rec, best_Sol, total_Val;
+float *fitness;
 
 
 int main(int argc, char *argv[]){
 
   int **colonia;
 
-  int num_Itens, num_Rec, best_Sol, total_Val;
   int cont = 0;
 
   char entrada[40];
 
   strcpy(entrada, argv[1]);
-  int n_Testes = atoi(argv[2]);
-  int n_Colonias = atoi(argv[3]);
+  int num_Testes = atoi(argv[2]);
+  int num_Colonias = atoi(argv[3]);
 
   arqIn = fopen(entrada, "r");
 
@@ -53,40 +54,38 @@ int main(int argc, char *argv[]){
   //printf("teste");
   inserir_Sep(arqIn, num_Itens, num_Rec);
 
-  AlocaMatriz(num_Itens, n_Colonias);
+  AlocaMatriz();
   
-  ini_Colonia(n_Colonias, num_Itens);
+  ini_Colonia();
 
-  while(cont < n_Testes){
+  while(cont < num_Testes){
 
-    checa_Validade(n_Colonias, num_Itens, num_Rec);
+    checa_Validade();
 
-      fit(n_Colonias, num_Itens);
-
-      prep(n_Colonias);
+      prep();
 
     cont++;
   }
 
-  LiberaMatriz(n_Colonias);
+  LiberaMatriz();
 
   fclose(arqIn);
 
 }
 
-void inserir_Sep(FILE *arquivo, int n_Itens, int n_Rec){ // função está dando falha na segmentação
+void inserir_Sep(FILE *arquivo){ // função está dando falha na segmentação
 
-  tp_Recurso = (int *) malloc(n_Itens * sizeof(int));
-  p_Recurso = (int *) malloc((n_Itens * n_Rec) * sizeof(int));
+  tp_Recurso = (int *) malloc(num_Itens * sizeof(int));
+  p_Recurso = (int *) malloc((num_Itens * n_Rec) * sizeof(int));
   lim_Recurso = (int *) malloc(n_Rec * sizeof(int));
 
-  for (int j = 0; j < n_Itens; j++)
+  for (int j = 0; j < num_Itens; j++)
 	  fscanf(arquivo, "%d", &(tp_Recurso[j]));
 
   //Leitura da matriz de pesos
   for (int j = 0; j < n_Rec; j++){
-	  for (int k = 0; k < n_Itens; k++){
-	    fscanf(arquivo, "%d", &(p_Recurso[j * n_Itens + k]));
+	  for (int k = 0; k < num_Itens; k++){
+	    fscanf(arquivo, "%d", &(p_Recurso[j * num_Itens + k]));
     }
   }
 
@@ -95,23 +94,23 @@ void inserir_Sep(FILE *arquivo, int n_Itens, int n_Rec){ // função está dando
 	  fscanf(arquivo, "%d", &(lim_Recurso[j]));
 }
 
-void AlocaMatriz(int num_Itens, int n_Colonias){
+void AlocaMatriz(){
 
   int i, j;
 
-  colonia = (int**) malloc(n_Colonias * sizeof(int*));
+  colonia = (int**) malloc(num_Colonias * sizeof(int*));
 
-  for(i = 0; i < n_Colonias; i++){
+  for(i = 0; i < num_Colonias; i++){
     colonia[i] = (int*) malloc(num_Itens * sizeof(int));
   }
 
 }
 
-void LiberaMatriz(int n_Colonias){
+void LiberaMatriz(){
 
 
   //Libera cada linha da matriz
-  for (int i = 0; i < n_Colonias; i++)
+  for (int i = 0; i < num_Colonias; i++)
 	free(colonia[i]);
 
   //Libera a matriz
@@ -123,22 +122,25 @@ void LiberaMatriz(int n_Colonias){
 
 }
 
-void ini_Colonia(int tamanho, int n_Itens){
+void ini_Colonia(){
   int i, j;
+
+  fitness = (float*) malloc(num_Colonias * sizeof(float));
 
   srand(time(NULL));
 
   for (i = 0; i < tamanho; i++){
-	  for (j = 0; j < n_Itens; j++){
+	  for (j = 0; j < num_Itens; j++){
       colonia[i][j] = rand() % 2;
       //printf("%d ", cabeca[i][j]);
     }
+    calcula_Fitness(i);
     //printf("\n");
   }
 }
 
-void checa_Validade(int tamanho, int n_Itens, int n_Rec){
-  int aux[tamanho][n_Itens];
+void checa_Validade(){
+  int aux[tamanho][num_Itens];
   bool valido = true;
 
   for(int i = 0; i < tamanho; i++){
@@ -150,10 +152,10 @@ void checa_Validade(int tamanho, int n_Itens, int n_Rec){
   for(int i = 0; i < tamanho; i++){
     valido = true;
 
-    for(int j = 0; j < n_Itens; j++){
+    for(int j = 0; j < num_Itens; j++){
       if(colonia[i][j] == 1){
         for(int k = 0; k < n_Rec; k++){
-          aux[i][k] = aux[i][k] + p_Recurso[j + k * n_Itens];
+          aux[i][k] = aux[i][k] + p_Recurso[j + k * num_Itens];
         }
       }
     }
@@ -166,14 +168,14 @@ void checa_Validade(int tamanho, int n_Itens, int n_Rec){
 
 }
 
-void fit(int tamanho, int n_Itens){
+/*void calcula_Fitness(int index, int num_Itens){
 
   fit_Colonia = (int *) malloc(tamanho * sizeof(int));
 
   int i, j;
 
   for(i = 0; i < tamanho; i++){
-    for(j = 0; j < n_Itens; j++){
+    for(j = 0; j < num_Itens; j++){
       if(colonia[i][j] == 1){
         fit_Colonia[i] = fit_Colonia[i] + tp_Recurso[j];
       }
@@ -181,21 +183,35 @@ void fit(int tamanho, int n_Itens){
     }
     //printf("%d ", fit_Colonia[i]);  
   }
+}*/
+
+void calcula_Fitness(int index){
+
+  int i, desl;
+
+  desl = index + num_Itens;
+
+  fitness[index] = 0;
+
+  for(i = 0; i < num_Itens; i++){
+    fitness[index] += colonia[desl + i] * p_Recurso[i];
+  }
+
 }
 
-void prep(int n_Colonias){
+void prep(){
 
-  atual_Size = (float *) malloc(n_Colonias * sizeof(float));
-  update_cosc = (float *) malloc(n_Colonias * sizeof(float));
-  fric_surf = (float *) malloc(n_Colonias * sizeof(float));
+  atual_Size = (float *) malloc(num_Colonias * sizeof(float));
+  update_cosc = (float *) malloc(num_Colonias * sizeof(float));
+  fric_surf = (float *) malloc(num_Colonias * sizeof(float));
   int i;
   float aux;
 
-  for(i = 0; i < n_Colonias; i++){
+  for(i = 0; i < num_Colonias; i++){
     atual_Size[i] = 1;
   }
 
-  for(i = 0; i < n_Colonias; i++){
+  for(i = 0; i < num_Colonias; i++){
     update_cosc[i] =  (atual_Size[i] + 4*(fit_Colonia[i]))/
                       (atual_Size[i] + 2*(fit_Colonia[i]));
     aux = (3 * atual_Size[i])/(4 * M_1_PI);
@@ -205,9 +221,30 @@ void prep(int n_Colonias){
 
 }
 
-void size(int n_Colonias){
+void tournament_Select(){
+  int i, poolSize;
+  int *pool;
 
-  for(int i = 0; i < n_Colonias; i++){
+  pool = (int*) malloc(poolSize * sizeof(int));
+
+  for (i = 0; i < poolSize; i++){
+    pool[i] = rand() % 
+  }
+
+}
+
+void movement(){
+	int m[num_Colonias], k[num_Colonias], l[num_Colonias];
+	
+	for(int i = 1; i < num_Colonias; i++){
+		m[i] = 
+	}
+	
+}
+
+void size(){
+
+  for(int i = 0; i < num_Colonias; i++){
     atual_Size[i] = update_cosc[i] * atual_Size[i];
     //printf("%f ", atual_Size[i]);
   }
